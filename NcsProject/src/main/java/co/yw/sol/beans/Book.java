52c5a54734de891f0f4f024da768b8fae6f06698
admoonistrator <91christian83@gmail.com>
2022-08-06 14:58:31 +0900

    book.java user.java

--- NcsProject/src/main/java/co/yw/sol/beans/Book.java
+++ NcsProject/src/main/java/co/yw/sol/beans/Book.java
@@ -2,6 +2,36 @@ package co.yw.sol.beans;
 
 public class Book {
 	
+	private String bookName;
+	private int bookPrice;
+	private String author;
+	private int cnt;
+	
+	
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
 	   
 	
 }
--- NcsProject/src/main/java/co/yw/sol/beans/User.java
+++ NcsProject/src/main/java/co/yw/sol/beans/User.java
@@ -6,6 +6,36 @@ import javax.validation.constraints.Email;
 
 public class User {
 	
+	@NotBlank
+	private String id;
+	@Email
+	private String email;
+	
+	private String pw;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
 	   
 		
 }
